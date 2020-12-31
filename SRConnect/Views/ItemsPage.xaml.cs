12fc0890dc670b687b5541e09c1666c81b854803
yossaj <yossaj@icloud.com>
2020-12-31 13:30:23 +0000

    permission check and request added

--- SRConnect/Views/ItemsPage.xaml.cs
+++ SRConnect/Views/ItemsPage.xaml.cs
@@ -13,6 +13,7 @@ using SRConnect.ViewModels;
 using MvvmCross.Forms.Views;
 using MvvmCross.Forms.Presenters.Attributes;
 using SRConnect.Services;
+using Xamarin.Essentials;
 
 namespace SRConnect.Views
 {
@@ -26,17 +27,39 @@ namespace SRConnect.Views
 
         }
 
-        void OnItemSelected(object sender, EventArgs args)
+        async void OnItemSelected(object sender, EventArgs args)
         {
-            Xamarin.Forms.DependencyService.Get<IWifiConnect>().ConnectToWifi("", "");
-            //var layout = (BindableObject)sender;
-            //var item = (Item)layout.BindingContext;
-            //await Navigation.PushAsync(new ItemDetailPage(new ItemDetailViewModel(item)));
+            bool hasLocationPermission = await CheckLocationPermission();
+            if (hasLocationPermission)
+            {
+                Xamarin.Forms.DependencyService.Get<IWifiConnect>().ConnectToWifi("", "");
+            }
+            else
+            {
+                await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+                if(await CheckLocationPermission())
+                {
+                    Xamarin.Forms.DependencyService.Get<IWifiConnect>().ConnectToWifi("", "");
+                }
+                else
+                {
+                    await Application.Current.MainPage.DisplayAlert(
+                        "Permission Not Granted",
+                        "Location Permission Needs to be Granted In Order to Scan Networks",
+                        "OK");
+                }
+            }
         }
 
         async void AddItem_Clicked(object sender, EventArgs e)
         {
-            await Navigation.PushModalAsync(new NavigationPage(new NewItemPage()));
+          await Navigation.PushModalAsync(new NavigationPage(new NewItemPage()));
+             }
+
+        public async Task<bool> CheckLocationPermission()
+        {
+            var status = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+            return status == PermissionStatus.Granted;
         }
 
     }
