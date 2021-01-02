1706132936cac230deafd5da2783a1382aea97eb
yossaj <yossaj@icloud.com>
2021-01-02 16:51:04 +0000

    Wifi Network model added

---
+++ SRConnect/Models/WifiNetwork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace SRConnect.Models
+{
+    public class WifiNetwork
+    {
+        public string SSID { get; set; }
+        public string NetworkPass { get; set; }
+        public int NetworkId { get; set;}
+        public bool Connected { get; set; }
+        public bool Connecting { get; set; }
+        public bool Saved { get; set; }
+        public bool DeviceName { get; set; }
+    }
+}
